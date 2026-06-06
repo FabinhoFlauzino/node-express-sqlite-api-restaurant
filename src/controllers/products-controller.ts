13722e63f5bdeb45a80778166a85af31dee09601
FabinhoFlauzino <fabinhoflauzino.dev@gmail.com>
2026-06-06 17:47:20 -0300

    Conexão, validação de dados produtos e inserçãod e dados na tabela

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,14 +1,32 @@
-import {Response, Request, NextFunction} from 'express';
-import { AppError } from '../utils/app-error';
+import { Response, Request, NextFunction } from "express";
+import { z } from "zod";
+import { knex } from "../database/knex";
 
 class ProductsController {
-    async index(request: Request, response: Response, next: NextFunction) {
-        try { 
-            return response.json({message: 'OK'});
-        } catch (error) {
-            next(error);
-        }
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      return response.json({ message: "OK" });
+    } catch (error) {
+      next(error);
     }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      await knex<ProductRepository>("products").insert({ name, price });
+
+      return response.status(201).json();
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { ProductsController };
